e9ebd81273f5e819d56e2800baec772768946f38
audio35444 <juaninsis@gmail.com>
2018-03-27 00:30:41 -0300

    'fixCommandParams'

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ module.exports.trend = require('./model/trend-ml.js');
 if (require.main === module) {
   // console.log('hola mundoooo');
   let argv = process.argv.slice(2);
+  if(!argv || argv.length<=0)argv = process.title.replace(/[\s\S]*meliapi/,'').trim().split(" ")
   if(argv[0]==="site") this.site.getSites().then( result => jsonToPrettyTable(result));
   else if(argv[0].length === 3 && argv[0].indexOf('M')!=-1) this.category.getCategoriesML(argv[0]).then(result => jsonToPrettyTable(result));
   else if(argv[0] === "trend") this.trend.getTrendsBySite(argv[1]?argv[1]:"MLA").then(result => jsonToPrettyTable(result));
